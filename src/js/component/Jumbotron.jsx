ccce9860ab8de5605e5c02762b0cceb32092caa2
PabloPh95 <pablowork95@gmail.com>
2025-02-03 13:25:37 +0000

    Jumbotron_Terminado

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
  export const Jumbotron = () => {
   return (
-    <div>Inserte aqui Jumbotron</div>
+    <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+          <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet, consecteur adipisicing elit. Ipsan, eligendi, in quo sunt possimus non incidunt odit
+            vero aliquid similique quaerat nam nobis illo aspermateur vitae fugiat numquam repellat.
+          </p>
+          <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
+        </div>
+      </div>
   )
 }
